5aba6a6c9008bc1dbdade24c066c617c7d68ae6c
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-13 19:19:41 +0200

    Task Exceptions-01

---
+++ Exceptions-01/Program.cs
@@ -0,0 +1,32 @@
+﻿string basePath = "../../../"; //since this executes in bin/Debug/net8.0 , this matches the project folder
+
+while (true)
+{
+    Console.Write("Enter a file name which you would like to read: ");
+    string? input = Console.ReadLine();
+
+    if (input == null || input.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+    {
+        break;
+    }
+
+    if (!File.Exists(basePath + input))
+    {
+        Console.WriteLine("File not found. Please try again.");
+        continue;
+    }
+
+    try
+    {
+        string fileContent = File.ReadAllText(basePath + input);
+        Console.WriteLine("File read. Content: \n" + fileContent);
+    }
+    catch (FileNotFoundException)
+    {
+        Console.WriteLine("File not found. Please try again.");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Something went wrong. Please try again.");
+    }
+}
---
+++ Exceptions-01/README.md
@@ -0,0 +1,12 @@
+﻿# Task
+
+Build a console app that reads files.
+- User inputs a file name in the terminal
+- If the file doesn't exist it prints "File doesn't exist. Please try again."
+
+# Zadatak
+
+Napravi aplikaciju za čitanje datoteka​
+- Korisnik upisuje ime datoteke u terminal
+- Ako datoteka ne postoji ispiše poruku "File doesn't exist. Please try again."​
+- Ako datoteka postoji, ispisuje njen sadržaj
