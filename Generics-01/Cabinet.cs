bfbd8c9318456d1252fe94c275c4e8b872b3645e
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-14 00:07:55 +0200

    Task Generics-01

---
+++ Generics-01/Cabinet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_01
+{
+    internal class Cabinet<T> where T : class
+    {
+        private T?[] _shelfs;
+
+        public Cabinet(int numberOfShelfs)
+        {
+            if (numberOfShelfs < 0)
+            {
+                throw new ArgumentException("Number of shelfs in a cabinet cannot be 0 or less");
+            }
+
+            _shelfs = new T[numberOfShelfs];
+        }
+
+        public void AddItem(T item, int position)
+        {
+            if (position < 0 || position >= _shelfs.Length)
+            {
+                throw new ArgumentOutOfRangeException("position", "Trying to add item to a shelf that is out of bounds of the cabinet size.");
+            }
+
+            if (_shelfs[position] != null)
+            {
+                throw new ArgumentException($"The shelf on position {position} is already taken.");
+            }
+
+            _shelfs[position] = item;
+        }
+
+        public T? RemoveItem(int position)
+        {
+            if (position < 0 || position >= _shelfs.Length)
+            {
+                throw new ArgumentOutOfRangeException("position", "Trying to remove an item from a shelf that is out of bounds of the cabinet size.");
+            }
+
+            T? item = _shelfs[position];
+            _shelfs[position] = null;
+
+            return item;
+        }
+
+        public T? ViewItem(int position)
+        {
+            return _shelfs[position];
+        }
+
+    }
+}
---
+++ Generics-01/README.md
@@ -0,0 +1,23 @@
+﻿# Task
+
+Create a generic collection "Cabinet", which has several shelfs. Each shelf can contain only one item.
+
+## Cabinet
+- The Constructor accepts an integer which initializes a cabinet with a certain number of shelfs
+- Method `Add(item, position)` puts an item on the shelf if the shelf is empty
+- Method `Remove(position)` remove an item from the shelf
+- Method `View(position)` returns the item from the shelf
+
+***
+
+# Zadatak
+
+Izradi generičku kolekciju "Cabinet", koja ima nekoliko polica, a u svaku policu može stati samo jedna stvar.​
+
+### Cabinet
+- Konstruktor prima `int` koji inlicijalizira kabinet s određenim brojem ladica​
+- Metoda `Add(item, position)` stavlja stvar u policu ako je polica prazna​
+- Metoda `Remove(position)` izvadi stvar iz police​
+- Motoda `View(position)` prikaže stvar iz police​
+
+​
