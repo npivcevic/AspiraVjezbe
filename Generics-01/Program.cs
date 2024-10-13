0527a1d5c2bc6d93d155c56952b4c2f2cfa8fd04
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-14 00:07:55 +0200

    Task Generics-01

---
+++ Generics-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using Generics_01;
+
+Cabinet<string> cabinet = new Cabinet<string>(5);
+
+cabinet.AddItem("hammer", 1);
+
+//cabinet.RemoveItem(1);
+string? s = cabinet.RemoveItem(1);
+
+if (s != null)
+{
+    Console.WriteLine(s);
+} else
+{
+    Console.WriteLine("Shelf is empty!");
+}
