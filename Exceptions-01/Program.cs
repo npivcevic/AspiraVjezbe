5b63aad8c8b7c1584dc18418979e6e4d73e7bb64
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-13 19:19:41 +0200

    Task Exceptions-01

---
+++ Exceptions-01/Program.cs
@@ -0,0 +1,38 @@
+﻿string basePath = "../../../"; //since this executes in bin/Debug/net8.0 , this matches the project folder
+
+while (true)
+{
+    Console.Write("Enter a file name which you would like to read: ");
+    string? input = Console.ReadLine();
+
+    if (String.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Input is empty. Pleaase try again.");
+        continue;
+    }
+
+    if (input == "exit")
+    {
+        break;
+    }
+
+    if (!File.Exists(basePath + input))
+    {
+        Console.WriteLine("File not found. Please try again.");
+        continue;
+    }
+
+    try
+    {
+        string fileContent = File.ReadAllText(basePath + input);
+        Console.WriteLine("File read. Content: \n" + fileContent);
+    }
+    catch (FileNotFoundException)
+    {
+        Console.WriteLine("(EXCEPTION) File not found. Please try again.");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Something went wrong. Please try again.");
+    }
+}
