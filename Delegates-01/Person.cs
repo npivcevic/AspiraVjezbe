0f5a3023484b3ad8391e937c65e738039d56904b
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-19 22:55:12 +0200

    Task Delegates-01

---
+++ Delegates-01/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_01
+{
+    internal class Person
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; } = 0;
+    }
+}
---
+++ Delegates-01/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Delegates_01;
+
+List<Person> people = new List<Person>
+{
+    new Person() { Name = "John", Age = 18 },
+    new Person() { Name = "Frank", Age = 38 },
+    new Person() { Name = "Anna", Age = 50 },
+    new Person() { Name = "Felicia", Age = 22 },
+    new Person() { Name = "Mike", Age = 90 },
+    new Person() { Name = "Karen", Age = 54 },
+};
+
+List<Person> olderThen30 = people.Where(p => p.Age >= 30).ToList();
+List<string> names = olderThen30.Select(p => p.Name).ToList();
+
+Console.WriteLine("People over 30:");
+names.ForEach(name => Console.WriteLine(name));
