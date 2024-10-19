f3372e1ee70718eb5e5f390f000189ee6350a1db
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-20 01:56:35 +0200

    Task Async-01

---
+++ Async-01/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+Task<string> task1 = DownloadDataAsync1();
+Task<string> task2 = DownloadDataAsync2();
+
+await Task.WhenAll(task1, task2);
+
+Console.WriteLine($"Tasks completed! Data: {task1.Result}, {task2.Result}");
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+static async Task<string> DownloadDataAsync1()
+{
+    await Task.Delay(6000);
+    return "data1";
+}
+
+static async Task<string> DownloadDataAsync2()
+{
+    await Task.Delay(6000);
+    return "data2";
+}
