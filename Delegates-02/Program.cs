43f0f233754dcdeeabe2c5a04855329b05798e5c
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-19 22:55:43 +0200

    Task Delegates-02

---
+++ Delegates-02/Program.cs
@@ -0,0 +1,24 @@
+﻿using Delegates_02;
+
+
+EmailServiceAWSSES emailServiceAWSSES = new EmailServiceAWSSES();
+NotificationService notificationService = new NotificationService(emailServiceAWSSES.SendEmail);
+notificationService.SendEmailNotification("john@example.com", "Hello!");
+
+EmailServiceSMTP emailServiceSMTP = new EmailServiceSMTP();
+notificationService.SendEmail = emailServiceSMTP.SendEmail;
+notificationService.SendEmailNotification("john@example.com", "Hello!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
