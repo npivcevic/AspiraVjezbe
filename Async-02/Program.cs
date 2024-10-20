921c3490514b8943c1968706ef6db9a54f9ba4f9
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-10-20 02:00:24 +0200

    Task Async-02

---
+++ Async-02/Program.cs
@@ -0,0 +1,40 @@
+﻿string basePath = "../../../"; //since this executes in bin/Debug/net8.0 , this matches the project folder
+
+while (true)
+{
+    Console.Write("Enter a file name which you would like to read: ");
+    string? input = Console.ReadLine();
+
+    if (String.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Input is empty. Pleaase try again.");
+        continue;
+    }
+
+    if (input == "exit")
+    {
+        break;
+    }
+
+    if (!File.Exists(basePath + input))
+    {
+        Console.WriteLine("File not found. Please try again.");
+        continue;
+    }
+
+    try
+    {
+        Task<string> readFileTask = File.ReadAllTextAsync(basePath + input);
+        Console.WriteLine("Reading file...");
+        string fileContent = await readFileTask;
+        Console.WriteLine("File read. Content: \n" + fileContent);
+    }
+    catch (FileNotFoundException)
+    {
+        Console.WriteLine("(EXCEPTION) File not found. Please try again.");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Something went wrong. Please try again.");
+    }
+}
