0aae8caa5d87c916d8d7f018dba801eb5b8b72d8
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-11-17 13:01:46 +0100

    Task WebAPI-01

---
+++ WebAPI-01/Controllers/MoviesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using WebAPI_01.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        static List<Movie> Movies = new List<Movie>();
+
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Get()
+        {
+            return Ok(Movies);
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public Movie? Get(int id)
+        {
+            return Movies.FirstOrDefault(m => m.Id == id);
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] Movie value)
+        {
+            Movies.Add(value);
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Movie value)
+        {
+            Movie? movie = Movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                Movies.Add(value);
+                return;
+            }
+            movie.Title = value.Title;
+            movie.Year = value.Year;
+
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Movie? movie = Movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return;
+            }
+            Movies.Remove(movie);
+        }
+    }
+}
---
+++ WebAPI-01/README.md
@@ -0,0 +1,40 @@
+﻿# Task
+
+Create a .NET Core Web API project and complete the following:
+
+- run the project, familiarize yourself with Swagger
+- delete the WeatherForcast controller and model
+- create a model `Movies` with props `Id`, `Title` and `Year`
+- create a new controller MoviesController
+	- scaffold the API Controller with read/write actions
+- add a static `List<Movie>` variable in the controller
+- implement all controller endpoints to get/add/update/remove movies
+- test all endpoints using Swagger
+- test all endpoints using Postman
+- in VS open the Endpoints Explorer
+	- View -> Other Windows -> Endpoints Explorer
+- create an .http file to test all endpoints
+
+***
+
+# Zadatak
+
+Kreirajte .NET Core Web API projekt i dovršite sljedeće:
+
+- pokrenite projekt i upoznajte se sa Swaggerom
+- obrišite `WeatherForecast` kontroler i model
+- kreirajte model `Movies` s svojstvima `Id`, `Title` i `Year`
+- kreirajte novi kontroler `MoviesController`
+	- scaffold-ajte API kontroler s read/write akcijama
+- dodajte statičku varijablu `List<Movie>` u kontroler
+- implementirajte sve endpointove kontrolera za dohvaćanje/dodavanje/ažuriranje/brisanje filmova
+- testirajte sve endpointove koristeći Swagger
+- testirajte sve endpointove koristeći Postman
+- u Visual Studiu otvorite Endpoints Explorer
+	- View -> Other Windows -> Endpoints Explorer
+- kreirajte `.http` datoteku za testiranje svih endpointova
+
+​​​
+​
+
+​
