00033d845596c5756e19d8cd56a041226d34d82f
Nikola Pivcevic <nikola.pivcevic@outlook.com>
2024-12-01 00:18:24 +0100

    Task WebAPIAuth-Basic

---
+++ WebAPIAuth-Basic/Auth/BasicAuthHandler.cs
@@ -0,0 +1,88 @@
+﻿
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using WebAPIAuth_Basic.Models;
+
+namespace BasicAuthenticationDemo.Models
+{
+    public class BasicAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        static List<User> users = new List<User>
+        {
+            new User(){ Id = 1, Email="john@example.com", Password = "secret", Role = "user" },
+            new User(){ Id = 1, Email="anna@example.com", Password = "password", Role = "admin" },
+        };
+
+        public BasicAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            }
+
+            User? user;
+            try
+            {
+                if (!AuthenticationHeaderValue.TryParse(Request.Headers["Authorization"], out var authHeader))
+                {
+                    return AuthenticateResult.Fail("Invalid Authorization Header Format");
+                }
+
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter ?? string.Empty);
+
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':', 2);
+
+                if (credentials.Length != 2)
+                {
+                    return AuthenticateResult.Fail("Invalid Authorization Header Content");
+                }
+
+                var username = credentials[0];
+                var password = credentials[1];
+
+                user = users.FirstOrDefault(u => u.Email == username && u.Password == password);
+            }
+            catch (FormatException)
+            {
+                return AuthenticateResult.Fail("Invalid Base64 Encoding in Authorization Header");
+            }
+            catch (Exception)
+            {
+                return AuthenticateResult.Fail("Error Processing Authorization Header");
+            }
+
+            if (user == null)
+            {
+                return AuthenticateResult.Fail("Invalid Username or Password");
+            }
+
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+
+        protected override async Task HandleChallengeAsync(AuthenticationProperties properties)
+        {
+            Response.Headers["WWW-Authenticate"] = "Basic realm=\"BasicAuthenticationDemo\", charset=\"UTF-8\"";
+            Response.StatusCode = 401;
+
+            await Response.WriteAsync("You need to authenticate to access this resource.");
+        }
+    }
+}
---
+++ WebAPIAuth-Basic/README.md
@@ -0,0 +1,31 @@
+﻿# Task
+
+Create a .NET Core Web API project that uses Basic Authentication
+
+- Create the model `User` with properties `Id`, `Email` and `Password`
+- Create the class `BasicAuthHandler` that inherits from ` AuthenticationHandler<AuthenticationSchemeOptions>`
+	- override the method `HandleAuthenticateAsync`, implement logic how to pull the username and passowrd form the http header, verify agains a local static list of users, and return a success Authentication ticket if user is found
+	- override the method `HandleChallengeAsync` that adds the http response header that indicates that Basic auth is required which prompts the browser to prompt the user to ender a username and password
+- Add the authorization service to our Services in Program.cs
+- Add the `[Authorize]` data attribute on the `WeatherForcaset` controller
+- OPTIONAL:
+	- add an additional parameter `Role` to the User model
+	- in `HandleAuthenticateAsync` add an additional Claim "Role" and populate it with the users role
+	- in the `[Authorize]` data attrubute add that only users with the Admin role can access the WeatherForecast controller
+
+***
+
+# Zadatak
+
+Kreirajte .NET Core Web API projekt koji koristi Basic Authentication
+
+- Kreirajte model `User` s atributima `Id`, `Email` i `Password`
+- Kreirajte klasu `BasicAuthHandler` koja nasljeđuje `AuthenticationHandler<AuthenticationSchemeOptions>`
+  - nadjačajte metodu `HandleAuthenticateAsync`, implementirajte logiku za dohvaćanje korisničkog imena i lozinke iz HTTP zaglavlja, provjeru prema lokalnoj statičkoj listi korisnika i vraćanje uspješne **Authentication ticket** ako je korisnik pronađen
+  - nadjačajte metodu `HandleChallengeAsync` koja dodaje HTTP odgovor sa zaglavljem koje označava da je potrebna Basic autentifikacija, što će uzrokovati da preglednik prikaže dijalog za unos korisničkog imena i lozinke
+- Dodajte servis za autorizaciju u `Services` unutar `Program.cs`
+- Dodajte `[Authorize]` atribut na `WeatherForecast` kontroler
+- OPCIONALNO:
+  - dodajte dodatni parametar `Role` u model `User`
+  - unutar `HandleAuthenticateAsync` dodajte dodatni **Claim** "Role" i popunite ga korisnikovom rolom
+  - u `[Authorize]` atribut dodajte da samo korisnici s Admin rolom mogu pristupiti `WeatherForecast` kontroleru
